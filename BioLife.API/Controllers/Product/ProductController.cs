34db47901bd90d52bef1b8b60c7a800d9165c6b0
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2024-08-31 08:06:37 +0700

    Clone from hulotoys

---
+++ BioLife.API/Controllers/Product/ProductController.cs
@@ -0,0 +1,198 @@
+﻿using Entities.ViewModels.Products;
+using HuloToys_Front_End.Models.Products;
+using HuloToys_Service.ElasticSearch;
+using HuloToys_Service.Models.APIRequest;
+using HuloToys_Service.MongoDb;
+using HuloToys_Service.RedisWorker;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Utilities;
+using Utilities.Contants;
+
+namespace WEB.CMS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDetailMongoAccess _productDetailMongoAccess;
+        private readonly CartMongodbService _cartMongodbService;
+        private readonly IConfiguration _configuration;
+        private readonly RedisConn _redisService;
+        private readonly GroupProductESService groupProductESService;
+
+        public ProductController(IConfiguration configuration, RedisConn redisService)
+        {
+            _productDetailMongoAccess = new ProductDetailMongoAccess(configuration);
+            _cartMongodbService = new CartMongodbService(configuration);
+            groupProductESService = new GroupProductESService(configuration["DataBaseConfig:Elastic:Host"], configuration);
+
+            _configuration = configuration;
+            _redisService = new RedisConn(configuration);
+            _redisService.Connect();
+        }
+
+        [HttpPost("get-list")]
+        public async Task<IActionResult> ProductListing([FromBody] APIRequestGenericModel input)
+        {
+            try
+            {
+                JArray objParr = null;
+                if (input != null && input.token != null && CommonHelper.GetParamWithKey(input.token, out objParr, _configuration["KEY:private_key"]))
+                {
+                    var request = JsonConvert.DeserializeObject<ProductListRequestModel>(objParr[0].ToString());
+                    if (request == null)
+                    {
+                        return Ok(new
+                        {
+                            status = (int)ResponseType.FAILED,
+                            msg = ResponseMessages.DataInvalid
+                        });
+                    }
+                    var cache_name = CacheType.PRODUCT_LISTING + (request.keyword ?? "") + request.group_id + request.page_index + request.page_size;
+                    var j_data = await _redisService.GetAsync(cache_name, Convert.ToInt32(_configuration["Redis:Database:db_search_result"]));
+                    if (j_data != null && j_data.Trim() != "")
+                    {
+                        ProductListResponseModel result = JsonConvert.DeserializeObject<ProductListResponseModel>(j_data);
+                        if (result != null && result.items != null)
+                        {
+                            return Ok(new
+                            {
+                                status = (int)ResponseType.SUCCESS,
+                                msg = ResponseMessages.Success,
+                                data = result
+                            });
+                        }
+                    }
+                    if (request.page_size <= 0) request.page_size = 10;
+                    if (request.page_index < 1) request.page_index = 1;
+                    var data = await _productDetailMongoAccess.ResponseListing(request.keyword, request.group_id, request.page_index, request.page_size);
+                   
+                    if (data != null  && data.items.Count > 0)
+                    {
+                        _redisService.Set(cache_name, JsonConvert.SerializeObject(data), Convert.ToInt32(_configuration["Redis:Database:db_search_result"]));
+                    }
+                    return Ok(new
+                    {
+                        status = (int)ResponseType.SUCCESS,
+                        msg = ResponseMessages.Success,
+                        data = data
+                    });
+                }
+
+
+            }
+            catch
+            {
+
+            }
+            return Ok(new
+            {
+                status = (int)ResponseType.FAILED,
+                msg = ResponseMessages.DataInvalid,
+            });
+        }
+
+
+        [HttpPost("detail")]
+        public async Task<IActionResult> ProductDetail([FromBody] APIRequestGenericModel input)
+        {
+            try
+            {
+                JArray objParr = null;
+                if (input != null && input.token != null && CommonHelper.GetParamWithKey(input.token, out objParr, _configuration["KEY:private_key"]))
+                {
+                    var request = JsonConvert.DeserializeObject<ProductDetailRequestModel>(objParr[0].ToString());
+                    if (request == null || request.id == null || request.id.Trim() == "")
+                    {
+                        return Ok(new
+                        {
+                            status = (int)ResponseType.FAILED,
+                            msg = ResponseMessages.DataInvalid
+                        });
+                    }
+                    var cache_name = CacheType.PRODUCT_DETAIL + request.id;
+                    var j_data = await _redisService.GetAsync(cache_name, Convert.ToInt32(_configuration["Redis:Database:db_search_result"]));
+                    if (j_data != null && j_data.Trim() != "")
+                    {
+                        ProductDetailResponseModel result = JsonConvert.DeserializeObject<ProductDetailResponseModel>(j_data);
+                        if (result != null)
+                        {
+                            return Ok(new
+                            {
+                                status = (int)ResponseType.SUCCESS,
+                                msg = ResponseMessages.Success,
+                                data = result
+                            });
+                        }
+                    }
+                    var data = await _productDetailMongoAccess.GetFullProductById(request.id);
+                    if (data != null)
+                    {
+                        _redisService.Set(cache_name, JsonConvert.SerializeObject(data), Convert.ToInt32(_configuration["Redis:Database:db_search_result"]));
+                    }
+                    return Ok(new
+                    {
+                        status = (int)ResponseType.SUCCESS,
+                        msg = "Success",
+                        data = data
+                    });
+
+                }
+
+            }
+            catch
+            {
+
+            }
+            return Ok(new
+            {
+                status = (int)ResponseType.FAILED,
+                msg = "Failed",
+            });
+        }
+        [HttpPost("group-product")]
+        public async Task<IActionResult> GroupProduct([FromBody] APIRequestGenericModel input)
+        {
+            try
+            {
+                JArray objParr = null;
+                if (input != null && input.token != null && CommonHelper.GetParamWithKey(input.token, out objParr, _configuration["KEY:private_key"]))
+                {
+                    var request = JsonConvert.DeserializeObject<ProductListRequestModel>(objParr[0].ToString());
+                    if (request == null || request.group_id <= 0)
+                    {
+                        return Ok(new
+                        {
+                            status = (int)ResponseType.FAILED,
+                            msg = ResponseMessages.DataInvalid
+                        });
+                    }
+                    var data = groupProductESService.GetListGroupProductByParentId(request.group_id);
+                    return Ok(new
+                    {
+                        status = (int)ResponseType.SUCCESS,
+                        msg = ResponseMessages.Success,
+                        data = data
+                    });
+                }
+
+
+            }
+            catch
+            {
+
+            }
+            return Ok(new
+            {
+                status = (int)ResponseType.FAILED,
+                msg = ResponseMessages.DataInvalid,
+            });
+        }
+      
+    }
+
+}
---
+++ BioLife.API/MongoDb/ProductDetailMongoAccess.cs
@@ -0,0 +1,207 @@
+﻿using Entities.ViewModels.Products;
+using HuloToys_Front_End.Models.Products;
+using HuloToys_Service.Utilities.constants.Product;
+using HuloToys_Service.Utilities.Lib;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace HuloToys_Service.MongoDb
+{
+    public class ProductDetailMongoAccess
+    {
+        private readonly IConfiguration _configuration;
+        private IMongoCollection<ProductMongoDbModel> _productDetailCollection;
+
+        public ProductDetailMongoAccess(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            string url = "mongodb://" + configuration["DataBaseConfig:MongoServer:Host"] + "";
+            var client = new MongoClient("mongodb://" + configuration["DataBaseConfig:MongoServer:Host"] + "");
+            IMongoDatabase db = client.GetDatabase(configuration["DataBaseConfig:MongoServer:catalog_core"]);
+            _productDetailCollection = db.GetCollection<ProductMongoDbModel>("ProductDetail");
+        }
+        public async Task<string> AddNewAsync(ProductMongoDbModel model)
+        {
+            try
+            {
+                model.GenID();
+                await _productDetailCollection.InsertOneAsync(model);
+                return model._id;
+            }
+            catch (Exception ex)
+            {
+                string error_msg = Assembly.GetExecutingAssembly().GetName().Name + "->" + MethodBase.GetCurrentMethod().Name + "=>" + ex.Message;
+                LogHelper.InsertLogTelegramByUrl(_configuration["telegram:log_try_catch:bot_token"], _configuration["telegram:log_try_catch:group_id"], error_msg);
+                return null;
+            }
+        }
+        public async Task<string> UpdateAsync(ProductMongoDbModel model)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.And(
+                    filter.Eq("_id", model._id));
+                await _productDetailCollection.FindOneAndReplaceAsync(filterDefinition, model);
+                return model._id;
+            }
+            catch (Exception ex)
+            {
+                string error_msg = Assembly.GetExecutingAssembly().GetName().Name + "->" + MethodBase.GetCurrentMethod().Name + "=>" + ex.Message;
+                LogHelper.InsertLogTelegramByUrl(_configuration["telegram:log_try_catch:bot_token"], _configuration["telegram:log_try_catch:group_id"], error_msg);
+                return null;
+            }
+        }
+
+
+        public async Task<ProductMongoDbModel> GetByID(string id)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x._id, id); ;
+                var model = await _productDetailCollection.Find(filterDefinition).FirstOrDefaultAsync();
+                return model;
+            }
+            catch (Exception ex)
+            {
+                string error_msg = Assembly.GetExecutingAssembly().GetName().Name + "->" + MethodBase.GetCurrentMethod().Name + "=>" + ex.Message;
+                LogHelper.InsertLogTelegramByUrl(_configuration["telegram:log_try_catch:bot_token"], _configuration["telegram:log_try_catch:group_id"], error_msg);
+                return null;
+            }
+        }
+        public async Task<ProductDetailResponseModel> GetFullProductById(string id)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x._id, id); ;
+                var model = await _productDetailCollection.Find(filterDefinition).FirstOrDefaultAsync();
+                var result = new ProductDetailResponseModel()
+                {
+                    product_main=model,
+                    product_sub=await SubListing(id)
+                };
+                return result;
+            }
+            catch (Exception ex)
+            {
+                string error_msg = Assembly.GetExecutingAssembly().GetName().Name + "->" + MethodBase.GetCurrentMethod().Name + "=>" + ex.Message;
+                LogHelper.InsertLogTelegramByUrl(_configuration["telegram:log_try_catch:bot_token"], _configuration["telegram:log_try_catch:group_id"], error_msg);
+                return null;
+            }
+        }
+
+        public async Task<List<ProductMongoDbModel>> Listing(string keyword = "", int group_id = -1, int page_index = 1, int page_size = 10)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                if(keyword!=null && keyword.Trim() != "")
+                {
+                    filterDefinition &= Builders<ProductMongoDbModel>.Filter.Regex(x => x.name, keyword);
+
+                }
+                if (group_id > 0)
+                {
+                    filterDefinition &= Builders<ProductMongoDbModel>.Filter.Regex(x => x.group_product_id, group_id.ToString());
+                }
+                var sort_filter = Builders<ProductMongoDbModel>.Sort;
+                var sort_filter_definition = sort_filter.Descending(x => x.updated_last);
+                var model = _productDetailCollection.Find(filterDefinition);
+                model.Options.Skip = page_index < 1 ? 0 : (page_index - 1) * page_size;
+                model.Options.Limit = page_size;
+                var result = await model.ToListAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                string error_msg = Assembly.GetExecutingAssembly().GetName().Name + "->" + MethodBase.GetCurrentMethod().Name + "=>" + ex.Message;
+                LogHelper.InsertLogTelegramByUrl(_configuration["telegram:log_try_catch:bot_token"], _configuration["telegram:log_try_catch:group_id"], error_msg);
+                return null;
+            }
+        }
+        public async Task<ProductListResponseModel> ResponseListing(string keyword = "", int group_id = -1, int page_index = 1, int page_size = 10)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                if (keyword != null && keyword.Trim() != "")
+                {
+                    filterDefinition &= Builders<ProductMongoDbModel>.Filter.Regex(x => x.name, keyword);
+
+                }
+                if (group_id > 0)
+                {
+                    filterDefinition &= Builders<ProductMongoDbModel>.Filter.Regex(x => x.group_product_id, group_id.ToString());
+                }
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.parent_product_id, "");
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.status, (int)ProductStatus.ACTIVE);
+
+                var sort_filter = Builders<ProductMongoDbModel>.Sort;
+                var sort_filter_definition = sort_filter.Descending(x => x.updated_last);
+                var model = _productDetailCollection.Find(filterDefinition);
+                long count = await model.CountDocumentsAsync();
+                model.Options.Skip = page_index < 1 ? 0 : (page_index - 1) * page_size;
+                model.Options.Limit = page_size;
+
+                return new ProductListResponseModel()
+                {
+                    items = await model.ToListAsync(),
+                    count = count
+                };
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public async Task<List<ProductMongoDbModel>> SubListing(string parent_id)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.parent_product_id, parent_id);
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.status, (int)ProductStatus.ACTIVE); ;
+
+                var model = _productDetailCollection.Find(filterDefinition);
+                var result = await model.ToListAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                string error_msg = Assembly.GetExecutingAssembly().GetName().Name + "->" + MethodBase.GetCurrentMethod().Name + "=>" + ex.Message;
+                LogHelper.InsertLogTelegramByUrl(_configuration["telegram:log_try_catch:bot_token"], _configuration["telegram:log_try_catch:group_id"], error_msg);
+                return null;
+            }
+        }
+
+        public async Task<string> DeactiveByParentId(string id)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.parent_product_id, id);
+                var update = Builders<ProductMongoDbModel>.Update.Set(x => x.status, (int)ProductStatus.DEACTIVE);
+
+                var updated_item = await _productDetailCollection.UpdateManyAsync(filterDefinition, update);
+                return id;
+            }
+            catch (Exception ex)
+            {
+                string error_msg = Assembly.GetExecutingAssembly().GetName().Name + "->" + MethodBase.GetCurrentMethod().Name + "=>" + ex.Message;
+                LogHelper.InsertLogTelegramByUrl(_configuration["telegram:log_try_catch:bot_token"], _configuration["telegram:log_try_catch:group_id"], error_msg);
+            }
+            return null;
+
+        }
+
+    }
+}
