1dfc01581eb8ac4f2236ef75db20d8247eafe765
luongthiminhtam <52973027+luongthiminhtam@users.noreply.github.com>
2024-10-05 11:05:57 +0100

    no message

--- BioLife.API/obj/Debug/net7.0/BioLife.API.AssemblyInfo.cs
+++ BioLife.API/obj/Debug/net7.0/BioLife.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioLife.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa498a076bb62a506f4738f9307ee3925df49775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496a4cfa56b00b15d4e0eecc26e753adb7866d42")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioLife.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioLife.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
